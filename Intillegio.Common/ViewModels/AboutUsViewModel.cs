3910f25ad2e5625d47e171aee68265fdc0375ddd
Google Google <Google@students.softuni.bg>
2019-09-07 20:42:10 +0300

    Added some static text in AboutUs

--- Intillegio.Common/ViewModels/AboutUsViewModel.cs
+++ Intillegio.Common/ViewModels/AboutUsViewModel.cs
@@ -6,5 +6,6 @@ namespace Intillegio.Common.ViewModels
     {
         public IEnumerable<SolutionViewModel> Solutions { get; set; }
         public IEnumerable<TeamMemberViewModel> TeamMembers { get; set; }
+        public Icons Icons { get; set; } = new Icons();
     }
 }
---
+++ Intillegio.Common/ViewModels/Icons.cs
@@ -0,0 +1,10 @@
+﻿namespace Intillegio.Common.ViewModels
+{
+   public enum Icons
+    {
+        target = 1,
+        networking,
+        diamond,
+        hourglass
+    }
+}
