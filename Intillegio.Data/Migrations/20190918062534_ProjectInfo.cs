4d973cf998a7cd709dd6b17637fb3dcc6af4be78
Google Google <Google@students.softuni.bg>
2019-09-18 10:11:40 +0300

    Removed constriction for 1000 symbols in ProjectInfo

---
+++ Intillegio.Data/Migrations/20190918062534_ProjectInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Intillegio.Data.Migrations
+{
+    public partial class ProjectInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectInfo",
+                table: "Projects",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectInfo",
+                table: "Projects",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ﻿This is a project about consultancy agency made primarily with ASP.Net Core, using MSSql database with code first approach.
-You can check it here www.intillegio.eu
+You can check the result here www.intillegio.eu
